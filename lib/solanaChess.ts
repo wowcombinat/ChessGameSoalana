c2275d9ced731f9de3efaf1d504840a2d558e4d4
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-02 19:53:41 +0200

    Update solanaChess.ts

--- lib/solanaChess.ts
+++ lib/solanaChess.ts
@@ -30,10 +30,43 @@ export async function joinGame(connection: web3.Connection, wallet: any, gamePub
   });
 }
 
-export async function makeMove(connection: web3.Connection, wallet: any, gamePublicKey: PublicKey, from: number, to: number) {
+export async function makeMove(connection: web3.Connection, wallet: any, gamePublicKey: PublicKey, from: number, to: number, promotion: string | null) {
   const program = await getProgram(connection, wallet);
 
-  await program.rpc.makeMove(from, to, {
+  await program.rpc.makeMove(from, to, promotion, {
+    accounts: {
+      game: gamePublicKey,
+      player: wallet.publicKey,
+    },
+  });
+}
+
+export async function resignGame(connection: web3.Connection, wallet: any, gamePublicKey: PublicKey) {
+  const program = await getProgram(connection, wallet);
+
+  await program.rpc.resign({
+    accounts: {
+      game: gamePublicKey,
+      player: wallet.publicKey,
+    },
+  });
+}
+
+export async function offerDraw(connection: web3.Connection, wallet: any, gamePublicKey: PublicKey) {
+  const program = await getProgram(connection, wallet);
+
+  await program.rpc.offerDraw({
+    accounts: {
+      game: gamePublicKey,
+      player: wallet.publicKey,
+    },
+  });
+}
+
+export async function acceptDraw(connection: web3.Connection, wallet: any, gamePublicKey: PublicKey) {
+  const program = await getProgram(connection, wallet);
+
+  await program.rpc.acceptDraw({
     accounts: {
       game: gamePublicKey,
       player: wallet.publicKey,
