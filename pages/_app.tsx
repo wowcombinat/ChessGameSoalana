532db7a1d8f37e9a91d9fc6266e7784ad134d74a
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-02 20:23:09 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import WalletConnectProvider from '../components/WalletConnectProvider';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl(network);
+  const wallets = [new PhantomWalletAdapter()];
+
   return (
-    <WalletConnectProvider>
-      <Component {...pageProps} />
-    </WalletConnectProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
