c95b0c72b43977d204e82a02e943a1c387255891
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-02 23:34:08 +0200

    Update anchor-client.ts

--- lib/anchor-client.ts
+++ lib/anchor-client.ts
@@ -1,4 +1,4 @@
-import { Connection, PublicKey } from '@solana/web3.js';
+iimport { Connection, PublicKey } from '@solana/web3.js';
 import { Program, AnchorProvider, web3 } from '@project-serum/anchor';
 import idl from './solana_chess.json';
 
