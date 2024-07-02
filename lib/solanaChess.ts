72697a6f5b9e8816fbc7eebfc6b8ea9e0aabec3e
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-03 00:57:52 +0200

    Update solanaChess.ts

--- lib/solanaChess.ts
+++ lib/solanaChess.ts
@@ -1,20 +1,79 @@
-import { Connection, PublicKey } from '@solana/web3.js';
-import { Program, web3, BN } from '@project-serum/anchor';
-import { getProgram } from './anchor-client';
-
-export async function initializeGame(connection: Connection, wallet: PublicKey, stake: number) {
-  const program = await getProgram(connection, wallet);
-  const game = web3.Keypair.generate();
-  const stakeAmount = new BN(stake * web3.LAMPORTS_PER_SOL);
-
-  await program.methods.initializeGame(stakeAmount)
-    .accounts({
-      game: game.publicKey,
-      player: wallet,
-      systemProgram: web3.SystemProgram.programId,
-    })
-    .signers([game])
-    .rpc();
-
-  return game.publicKey;
+{
+  "version": "0.1.0",
+  "name": "solana_chess",
+  "instructions": [
+    {
+      "name": "initializeGame",
+      "accounts": [
+        {
+          "name": "game",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "player",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "stake",
+          "type": "u64"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "Game",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "white",
+            "type": "publicKey"
+          },
+          {
+            "name": "black",
+            "type": "publicKey"
+          },
+          {
+            "name": "stake",
+            "type": "u64"
+          },
+          {
+            "name": "state",
+            "type": {
+              "defined": "GameState"
+            }
+          }
+        ]
+      }
+    }
+  ],
+  "types": [
+    {
+      "name": "GameState",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "WaitingForOpponent"
+          },
+          {
+            "name": "InProgress"
+          },
+          {
+            "name": "Finished"
+          }
+        ]
+      }
+    }
+  ]
 }
