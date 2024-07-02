a3b9e6bb49882072e643f9117ea483feb7297f7c
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-02 23:31:19 +0200

    Update anchor-client.ts

--- lib/anchor-client.ts
+++ lib/anchor-client.ts
@@ -1,5 +1,5 @@
 import { Connection, PublicKey } from '@solana/web3.js';
-import { Program, AnchorProvider, web3, utils, BN } from '@project-serum/anchor';
+import { Program, AnchorProvider, web3 } from '@project-serum/anchor';
 import idl from './solana_chess.json';
 
 const programID = new PublicKey('Hp8cLdkPkHkSW9PbXutVUmoDR8pWbrqXLRbY3TmVj5kn');
@@ -11,13 +11,14 @@ export function getProvider(connection: Connection, wallet: any) {
   const provider = new AnchorProvider(
     connection,
     wallet,
-    opts.preflightCommitment,
+    opts
   );
   return provider;
 }
 
 export async function getProgram(connection: Connection, wallet: any) {
   const provider = getProvider(connection, wallet);
+  // @ts-ignore
   const program = new Program(idl, programID, provider);
   return program;
 }
