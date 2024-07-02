dd9abca69aa22157a2496e74dba5eca883af197d
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-03 01:55:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      os: false,
-      path: false,
-    };
-    return config;
-  },
 }
 
 module.exports = nextConfig
